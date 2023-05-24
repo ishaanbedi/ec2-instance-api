88f0f72e172333bcc15ac122fb9afb9a0af6dfa5
Ishaan Bedi <bediishaan6@gmail.com>
2023-05-24 11:06:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const cr = require('crypto');
 const a = require('express')();
 
 a.get('/', (r, s) => {
+    console.log('/ hit');
     s.json({ m: 'endpoint at /e' });
 });
 
 a.get('/e', (r, s) => {
+    console.log('/e hit');
     const rN = cr.randomBytes(4).readUInt32BE(0);
     s.json({ en: rN });
 });
